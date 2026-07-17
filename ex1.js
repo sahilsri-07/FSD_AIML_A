92aa86b1737a14d98eaae54d6cca35758e16acbb
Sahil Srivastava <hellosahil07@gmail.com>
2026-07-17 14:21:18 +0530

    created ex1.js file

---
+++ ex1.js
@@ -0,0 +1,11 @@
+// let a = 10;
+// const b = 25;
+// console.log("a=",a);
+// console.log("b=",b);
+// b=a++;
+// console.log("b =",b);/
+
+const sum=(a,b)=>a+b;
+console.log("sum=",sum(10,20));
+console.log("sum=",sum(17,29));
+console.log("sum=",sum(15,35));
